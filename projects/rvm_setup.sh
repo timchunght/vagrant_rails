d36a0fd2b7348e1f689bf768a5ca905d1a9b6042
Timothy Chung <timchunght@gmail.com>
2015-09-25 18:03:11 -0400

    RVM not working yet

--- projects/rvm_setup.sh
+++ projects/rvm_setup.sh
@@ -2,7 +2,8 @@
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -L https://get.rvm.io | bash -s stable 
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.profile
+source ~/.profile
 # Install Ruby
-rvm install 2.1.1
-rvm use 2.1.1 --default
+# rvm install 2.1.1
+# rvm use 2.1.1 --default
 gem install bundler
